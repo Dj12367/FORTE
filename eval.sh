f6a39621b20dd16cd4325d499530bfedb29bc7ce
paul <paulj@gmail.com>
2024-09-28 16:44:11 +0530

    fix

--- eval.sh
+++ eval.sh
@@ -1,6 +1,7 @@
 export PYTHONPATH=".:/home/p20200470/miniconda3/envs/forte/bin/python"
 source /home/p20200470/miniconda3/etc/profile.d/conda.sh
 conda activate forte
+pip show wandb
 #This code capture the report output during the optimization process and record the report to an excel file (which should be specified in /pipeline/train/eval.py
 CUDA_VISIBLE_DEVICES=1 accelerate launch --config_file=./evaluation/pipeline/accelerate_configs/accelerate_config_fsdp.yaml \
 ./evaluation/pipeline/train/eval.py \
